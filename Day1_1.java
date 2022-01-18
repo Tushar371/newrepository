b5afcc8876a7efcad6a50b17b4a6a91981d3a411
tushar <tusharsutar371@gmail.com>
2022-01-18 12:11:52 +0530

    added

---
+++ Day1_1.java
@@ -0,0 +1,19 @@
+//import java.util.Scanner;
+
+import java.util.*;
+
+public class Day1_1 
+{
+
+	public static void main(String[] args)
+	{
+		Scanner sc= new Scanner(System.in);
+		int num1,num2;
+		System.out.println("enter first number :");
+		num1=sc.nextInt();
+		System.out.println("enter Sencond number :");
+		num2=sc.nextInt();
+		System.out.println("Addition ="+(num1+num2)+"\nSub="+(num1-num2)+"\nmulti ="+(num1*num2)+"\ndiv ="+((float)num1/num2));
+	}
+
+}
